3f815bb786883116755c2ff46b810c1a019cbdcf
Soulinall <soulinallmedia@gmail.com>
2020-05-06 21:16:32 +0530

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,16 @@
+function bclick(val){
+
+    console.log(val)
+    document.getElementById("screen").value= document.getElementById("screen").value+val;
+ }
+function clearall (){
+document.getElementById("screen").value=""
+
+}
+function equalclick(){
+var text=document.getElementById("screen").value
+var result=eval(text)
+document.getElementById("screen").value=result
+
+}
+     
